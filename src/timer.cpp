36f9d2178fc6ffda0d1fca350d53050d9fe43e2a
Student 03 Lab RoSo <lab_roso_stud03@kaon.int.rcs.ei.tum.de>
2016-11-04 13:55:09 +0100

    Include timer.cpp changes

--- src/timer.cpp
+++ src/timer.cpp
@@ -3,5 +3,5 @@
 bool Timer::timeout(){
     clock_gettime(CLOCK_MONOTONIC, &current_time);
 
-    return current_time.tv_sec - start_time.tv_sec >= timestamp;
+    return current_time.tv_nsec - start_time.tv_nsec >= timestamp;
 }
