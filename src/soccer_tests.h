8c0e9c86f0fec6d9745d5acc094bb66933f10972
Student 03 Lab RoSo <lab_roso_stud03@kaon.int.rcs.ei.tum.de>
2016-11-04 13:51:31 +0100

    fix merging

--- src/soccer_tests.h
+++ src/soccer_tests.h
@@ -47,6 +47,15 @@ private:
     Robot* red3;
 
 public:
+    /**
+     * @brief Soccer_Tests
+     * @param blue1_in
+     * @param blue2_in
+     * @param blue3_in
+     * @param red1_in
+     * @param red2_in
+     * @param red3_in
+     */
     Soccer_Tests(Robot* blue1_in, Robot* blue2_in, Robot* blue3_in,
                  Robot* red1_in, Robot* red2_in, Robot* red3_in);
 
--- src/timer.cpp
+++ src/timer.cpp
@@ -1,1 +1,7 @@
+#include "timer.h"
 
+bool Timer::timeout(){
+    clock_gettime(CLOCK_MONOTONIC, &current_time);
+
+    return current_time.tv_sec - start_time.tv_sec >= timestamp;
+}
