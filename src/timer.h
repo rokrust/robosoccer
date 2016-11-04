666790a8a5530cbba67fb86fa324968347cedf19
Student 03 Lab RoSo <lab_roso_stud03@kaon.int.rcs.ei.tum.de>
2016-11-04 13:47:56 +0100

    implement timer

---
+++ src/timer.h
@@ -0,0 +1,26 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <sys/time.h>
+#include <time.h>
+
+/**
+ * @brief The Timer class
+ */
+
+class Timer{
+private:
+    timespec start_time, current_time;
+    int utimestamp;
+
+public:
+    Timer(){;}
+    Timer(int time): utimestamp(time*1000){;} //convert uSec to nSec
+
+    void enable(){clock_gettime( CLOCK_MONOTONIC, &start_time); }
+
+    void set_timestamp(int timestamp){ utimestamp = timestamp*1000; }
+
+    bool timeout();
+};
+#endif // TIMER_H
